7f7aeeb4312ef483ef0a8b2ddeadfa9d848af7c0
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-01 14:18:42 +0200

    Created student database as array

--- src/databaseapp/StudentDatabaseApp.java
+++ src/databaseapp/StudentDatabaseApp.java
@@ -1,13 +1,25 @@
 package databaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.enroll();
-        student.payTuition();
-        System.out.println(student.showInfo());
         //Ask how many students to add
+        System.out.print("Enter number of student count to be added: ");
+        Scanner sc = new Scanner(System.in);
+        int studentCount = sc.nextInt();
+        Student[] students = new Student[studentCount];
 
         //Create new students
+        for(int i = 0; i < studentCount; i++) {
+            students[i] = new Student();
+            students[i].enroll();
+            students[i].payTuition();
+            System.out.println("-----");
+        }
+
+        for(Student student : students) {
+            System.out.println(student.showInfo() + "\n");
+        }
     }
 }
