15d45795757c3e47c6b1c50770659e8310f0dbcd
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-01 11:50:07 +0200

    Added requirements and variables

--- src/databaseapp/Student.java
+++ src/databaseapp/Student.java
@@ -1,4 +1,24 @@
 package databaseapp;
 
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String courses;
+    private String year;
+    private int studentId;
+    private int courseCost = 600;
+    private int tuitionBalance;
+
+    //Constructor asks for name and year
+
+    //generate id
+
+    //enroll in courses
+
+    //view balance
+
+    //pay tuition
+
+    //show status
 }
--- src/databaseapp/StudentDatabaseApp.java
+++ src/databaseapp/StudentDatabaseApp.java
@@ -1,5 +1,9 @@
 package databaseapp;
 
 public class StudentDatabaseApp {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        //Ask how many students to add
+
+        //Create new students
+    }
 }
