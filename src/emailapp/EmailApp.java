7756514940d8ccd4024c0337d6fde4881bdd9290
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-01 11:03:17 +0200

    Tested getters and setters in main

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,5 +4,11 @@ public class EmailApp {
 
     public static void main(String[] args) {
         Email email1 = new Email("Arthur", "Dent");
+
+        email1.setMailboxSize(1000);
+        email1.setAlternateEmail("a.dent@earth.com");
+        email1.changePassword("Trillian");
+
+        System.out.println("Your mailbox size is: " + email1.getMailboxSize() + ", your alternate e-mail is: " + email1.getAlternateEmail() + " and your password is: " + email1.getPassword());
     }
 }
