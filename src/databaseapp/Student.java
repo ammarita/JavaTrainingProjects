1560e162cde5f1a3ba2835a69aa7adf4ce8e73c5
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-01 13:13:31 +0200

    Added method for creating student's id

--- src/databaseapp/Student.java
+++ src/databaseapp/Student.java
@@ -7,10 +7,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private String courses;
+    private String studentId;
     private int classLevel;
-    private int studentId;
-    private int courseCost = 600;
     private int tuitionBalance;
+    private static int id = 1000;
+    private static int courseCost = 600;
 
     //Constructor asks for name and year
     public Student() {
@@ -21,11 +22,16 @@ public class Student {
         this.lastName = sc.nextLine();
         System.out.print("Enter student's class level\n(1 - Freshmen; 2 - Sophomore; 3 - Junior; 4 - Senior): ");
         this.classLevel = sc.nextInt();
+        setStudentId();
 
-        System.out.println(firstName + " " + lastName + " " + classLevel);
+        System.out.println(firstName + " " + lastName + " " + classLevel + " " + studentId);
     }
 
-    //generate id
+    //generate id that contains class level and 4 more digits
+    private void setStudentId() {
+        id++;
+        this.studentId = classLevel + "" + id;
+    }
 
     //enroll in courses
 
