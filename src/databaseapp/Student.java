60e1aed7f2ed550bed821af13ae987c1dfe67162
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-01 12:11:06 +0200

    Added student constructor

--- src/databaseapp/Student.java
+++ src/databaseapp/Student.java
@@ -1,16 +1,29 @@
 package databaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
     private String courses;
-    private String year;
+    private int classLevel;
     private int studentId;
     private int courseCost = 600;
     private int tuitionBalance;
 
     //Constructor asks for name and year
+    public Student() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter student's first name: ");
+        this.firstName = sc.nextLine();
+        System.out.print("Enter student's last name: ");
+        this.lastName = sc.nextLine();
+        System.out.print("Enter student's class level\n(1 - Freshmen; 2 - Sophomore; 3 - Junior; 4 - Senior): ");
+        this.classLevel = sc.nextInt();
+
+        System.out.println(firstName + " " + lastName + " " + classLevel);
+    }
 
     //generate id
 
--- src/databaseapp/StudentDatabaseApp.java
+++ src/databaseapp/StudentDatabaseApp.java
@@ -2,6 +2,7 @@ package databaseapp;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+        Student student = new Student();
         //Ask how many students to add
 
         //Create new students
