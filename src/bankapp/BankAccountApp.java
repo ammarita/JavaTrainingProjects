256f30c50e111f47429b5e98c6545bf12f9c1ffd
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-04 13:44:37 +0200

    Adding account object list

--- src/bankapp/BankAccountApp.java
+++ src/bankapp/BankAccountApp.java
@@ -1,31 +1,33 @@
 package bankapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
-
+        List<Account> accounts = new LinkedList<>();
         String file = "C:\\Users\\marita.lasmane\\Downloads\\NewBankAccounts.csv";
-
-//        CheckingAccount checking1 = new CheckingAccount("Thomas Edison", "123456789", 1500);
-//        SavingAccount savings1 = new SavingAccount("Alison Cooper", "987654321", 3456);
-//
-//        checking1.showInfo();
-//        System.out.println("---");
-//        savings1.showInfo();
-//
-//        savings1.compound();
-
-        //Read CSV and create new accounts corresponding that file
         List<String[]> newClients = utilities.CSV.read(file);
 
         for(String[] client : newClients ) {
-            System.out.println("NEW ACCOUNT");
-            System.out.println(client[0]);
-            System.out.println(client[1]);
-            System.out.println(client[2]);
-            System.out.println(client[3]);
+            String name = client[0];
+            String socSecurityNo = client[1];
+            String accountType = client[2];
+            double startingBalance = Double.parseDouble(client[3]);
+            System.out.println(name + " " + socSecurityNo + " " + accountType + " EUR " + startingBalance);
+
+            if(accountType.equals("Savings")) {
+                accounts.add(new SavingAccount(name, socSecurityNo, startingBalance));
+            } else if (accountType.equals("Checking")) {
+                accounts.add(new CheckingAccount(name, socSecurityNo, startingBalance));
+            } else {
+                System.out.println("Error Reading Account Type");
+            }
         }
 
+        for (Account acc : accounts) {
+            System.out.println("- - -");
+            acc.showInfo();
+        }
     }
 }
