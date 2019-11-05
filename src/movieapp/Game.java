e0270c336df7d006a05f5f3c23c1be6bc00bd57c
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-05 14:33:34 +0200

    Adding general methods for game

--- src/movieapp/Game.java
+++ src/movieapp/Game.java
@@ -9,14 +9,17 @@ import java.util.Scanner;
 public class Game {
     File file = new File("C:\\Users\\marita.lasmane\\Downloads\\JavaTrainingProjects\\src\\movieapp\\movies.txt");
     List<String> movieList = new ArrayList<>();
+    List<String> wrongGuesses = new ArrayList<>();
+    String movie = "";
 
     public Game() {
         createMovieList();
+        randomMovie();
     }
 
     //Creating movie list from file
     private void createMovieList() {
-                try {
+        try {
             Scanner sc = new Scanner(file);
             while (sc.hasNextLine()) {
                 movieList.add(sc.nextLine());
@@ -28,8 +31,23 @@ public class Game {
     }
 
     //Randomly pick a movie from list
-    public String randomMovie() {
+    private String randomMovie() {
         int randomPick = (int) (Math.random() * (movieList.size() -1));
-        return movieList.get(randomPick);
+        movie = movieList.get(randomPick);
+        return movie;
+    }
+
+    //Replace movie name with -
+    public String hideMovie() {
+        return movie.replaceAll(".", "-");
+    }
+
+    //Show wrong guesses
+    public String  showWrongGuesses() {
+        String wrongGuess = "";
+        for(String guess : wrongGuesses) {
+            wrongGuess = guess + " ";
+        }
+        return wrongGuess;
     }
 }
--- src/movieapp/GuessMovie.java
+++ src/movieapp/GuessMovie.java
@@ -8,7 +8,15 @@ public class GuessMovie {
         Game game = new Game();
 
         System.out.println("Guess The Movie");
-        
+        System.out.println(game.movie);
+        System.out.println(game.hideMovie());
+
+        while(game.wrongGuesses.size() < 10) {
+            System.out.println("Guess the Movie: " + game.hideMovie());
+            System.out.println("You have guessed (" + game.wrongGuesses.size() + ") wrong letters: " + game.wrongGuesses);
+            System.out.print("Guess a letter: ");
+            String guess = input.next();
+        }
 
     }//end main
 }
