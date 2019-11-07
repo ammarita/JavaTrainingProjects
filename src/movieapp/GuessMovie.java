ac3cf06623300cc127d214a651c6c555f1b0b6f2
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-07 14:45:26 +0200

    Added loop for games

--- src/movieapp/GuessMovie.java
+++ src/movieapp/GuessMovie.java
@@ -6,23 +6,29 @@ public class GuessMovie {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String guess = "";
-        Game game = new Game();
+        String playing = "y";
 
         System.out.println("Guess The Movie Game");
 
-        while(game.getWrongGuesses().size() < 10) {
-            System.out.println("Guess the Movie: " + game.getHiddenMovieName());
-            System.out.println("You have guessed (" + game.getWrongGuesses().size() + ") wrong letters: " + game.showWrongGuesses());
-            System.out.print("Guess a letter: ");
-            guess = input.next();
-            game.checkGuess(guess);
+        while(playing.equals("y")) {
+            Game game = new Game();
+            while (game.getWrongGuesses().size() < 10) {
+                System.out.println("Guess the Movie: " + game.getHiddenMovieName());
+                System.out.println("You have guessed (" + game.getWrongGuesses().size() + ") wrong letters: " + game.showWrongGuesses());
+                System.out.print("Guess a letter: ");
+                guess = input.next();
+                game.checkGuess(guess);
 
-            if(!game.isPlaying()) break;
-        }
+                if (!game.isPlaying()) break;
+            }
 
-        if(game.getWrongGuesses().size() >= 10) {
-            System.out.println("You lost!" +
-                    "\nYou did not guessed \'" + game.getMovie() + "\' correctly.");
+            if (game.getWrongGuesses().size() >= 10) {
+                System.out.println("You lost!" +
+                        "\nYou did not guessed \'" + game.getMovie() + "\' correctly.");
+            }
+
+            System.out.println("Play again? (y/n)");
+            playing = input.next();
         }
     }//end main
 }
