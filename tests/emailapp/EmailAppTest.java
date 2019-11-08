c6801bb7bccf615122f75af74a24dc504ba858e2
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-08 13:32:22 +0200

    Creaded tests package

---
+++ tests/emailapp/EmailAppTest.java
@@ -0,0 +1,22 @@
+package emailapp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
---
+++ tests/spacechallengeapp/SpaceChallengeAppTest.java
@@ -0,0 +1,22 @@
+package spacechallengeapp;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpaceChallengeAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
