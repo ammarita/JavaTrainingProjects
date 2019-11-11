01774c8f65712466fe047283d058ab415b36075c
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-11 13:37:24 +0200

    Created new battle ships project

---
+++ src/battleshipsapp/BattleShipsApp.java
@@ -0,0 +1,7 @@
+package battleshipsapp;
+
+public class BattleShipsApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/battleshipsapp/OceanMap.java
@@ -0,0 +1,13 @@
+package battleshipsapp;
+
+public class OceanMap {
+    char[][] mapGrid = new char[10][10];
+
+    public OceanMap() {
+
+    }
+
+    private void printMap() {
+
+    }
+}
