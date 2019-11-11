63b63fb532762f2e8c80aa405956c07de704239d
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-11 16:55:03 +0200

    Created new game loop

--- src/battleshipsapp/BattleShipsApp.java
+++ src/battleshipsapp/BattleShipsApp.java
@@ -1,7 +1,20 @@
 package battleshipsapp;
 
+import java.util.Scanner;
+
 public class BattleShipsApp {
     public static void main(String[] args) {
-        OceanMap oceanMap = new OceanMap();
+        Scanner sc = new Scanner(System.in);
+        boolean playing = true;
+        while (playing) {
+            OceanMap oceanMap = new OceanMap();
+
+            System.out.println("Do you want to play again? (y/n)");
+            String answer = sc.next().toLowerCase();
+
+            if (answer.equals("n")) {
+                playing = false;
+            }
+        }
     }//end main
 }
