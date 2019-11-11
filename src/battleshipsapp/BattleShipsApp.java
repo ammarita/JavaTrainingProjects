f74fe84b2409c7a4c0753743c81d8f017c815304
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-11 13:55:22 +0200

    Added method to display game map

--- src/battleshipsapp/BattleShipsApp.java
+++ src/battleshipsapp/BattleShipsApp.java
@@ -2,6 +2,6 @@ package battleshipsapp;
 
 public class BattleShipsApp {
     public static void main(String[] args) {
-
+        OceanMap oceanMap = new OceanMap();
     }
 }
--- src/battleshipsapp/OceanMap.java
+++ src/battleshipsapp/OceanMap.java
@@ -4,10 +4,20 @@ public class OceanMap {
     char[][] mapGrid = new char[10][10];
 
     public OceanMap() {
-
+        System.out.println("***** Welcome to Battle Ship Game *****");
+        System.out.println("\nRight now, the sea is empty.\n");
+        printMap();
     }
 
     private void printMap() {
-
+        System.out.println("123456789");
+        for(int i = 0; i < mapGrid.length; i++) {
+            System.out.print(i + " |");
+            for (int j = 0; j < mapGrid[j].length - 1; j++) {
+                System.out.print(mapGrid[i][j]);
+            }
+            System.out.println("| " + i);
+        }
+        System.out.println("123456789");
     }
 }
