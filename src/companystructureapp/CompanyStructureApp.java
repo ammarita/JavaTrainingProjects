e269c16fa25faecabd4bb92dd201e041bfd9fbcd
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-13 15:39:51 +0200

    Commented out unfinished project

--- src/companystructureapp/CompanyStructureApp.java
+++ src/companystructureapp/CompanyStructureApp.java
@@ -1,33 +1,33 @@
-package companystructureapp;
-
-//project description: https://courses.edx.org/courses/course-v1:Microsoft+DEV277x+3T2019/courseware/9c4e1dfcce774610938d51852584f4a1/b50bbd55c74747c883c8efd51757c8c9/1?activate_block_id=block-v1%3AMicrosoft%2BDEV277x%2B3T2019%2Btype%40vertical%2Bblock%4000dc29c26c21403cbdbc0ad8c5b0f640
-public class CompanyStructureApp {
-    public static void main(String[] args) {
-        TechnicalLead CTO = new TechnicalLead("Satya Nadella");
-        SoftwareEngineer seA = new SoftwareEngineer("Kasey");
-        SoftwareEngineer seB = new SoftwareEngineer("Breana");
-        SoftwareEngineer seC = new SoftwareEngineer("Eric");
-        CTO.addReport(seA);
-        CTO.addReport(seB);
-        CTO.addReport(seC);
-        System.out.println(CTO.getTeamStatus());
-
-        TechnicalLead VPofENG = new TechnicalLead("Bill Gates");
-        SoftwareEngineer seD = new SoftwareEngineer("Winter");
-        SoftwareEngineer seE = new SoftwareEngineer("Libby");
-        SoftwareEngineer seF = new SoftwareEngineer("Gizan");
-        SoftwareEngineer seG = new SoftwareEngineer("Zaynah");
-        VPofENG.addReport(seD);
-        VPofENG.addReport(seE);
-        VPofENG.addReport(seF);
-        VPofENG.addReport(seG);
-        System.out.println(VPofENG.getTeamStatus());
-
-        BusinessLead CFO = new BusinessLead("Amy Hood");
-        Accountant actA = new Accountant("Niky");
-        Accountant actB = new Accountant("Andrew");
-        CFO.addReport(actA, CTO);
-        CFO.addReport(actB, VPofENG);
-        System.out.println(CFO.getTeamStatus());
-    }
-}
+//package companystructureapp;
+//
+////project description: https://courses.edx.org/courses/course-v1:Microsoft+DEV277x+3T2019/courseware/9c4e1dfcce774610938d51852584f4a1/b50bbd55c74747c883c8efd51757c8c9/1?activate_block_id=block-v1%3AMicrosoft%2BDEV277x%2B3T2019%2Btype%40vertical%2Bblock%4000dc29c26c21403cbdbc0ad8c5b0f640
+//public class CompanyStructureApp {
+//    public static void main(String[] args) {
+//        TechnicalLead CTO = new TechnicalLead("Satya Nadella");
+//        SoftwareEngineer seA = new SoftwareEngineer("Kasey");
+//        SoftwareEngineer seB = new SoftwareEngineer("Breana");
+//        SoftwareEngineer seC = new SoftwareEngineer("Eric");
+//        CTO.addReport(seA);
+//        CTO.addReport(seB);
+//        CTO.addReport(seC);
+//        System.out.println(CTO.getTeamStatus());
+//
+//        TechnicalLead VPofENG = new TechnicalLead("Bill Gates");
+//        SoftwareEngineer seD = new SoftwareEngineer("Winter");
+//        SoftwareEngineer seE = new SoftwareEngineer("Libby");
+//        SoftwareEngineer seF = new SoftwareEngineer("Gizan");
+//        SoftwareEngineer seG = new SoftwareEngineer("Zaynah");
+//        VPofENG.addReport(seD);
+//        VPofENG.addReport(seE);
+//        VPofENG.addReport(seF);
+//        VPofENG.addReport(seG);
+//        System.out.println(VPofENG.getTeamStatus());
+//
+//        BusinessLead CFO = new BusinessLead("Amy Hood");
+//        Accountant actA = new Accountant("Niky");
+//        Accountant actB = new Accountant("Andrew");
+//        CFO.addReport(actA, CTO);
+//        CFO.addReport(actB, VPofENG);
+//        System.out.println(CFO.getTeamStatus());
+//    }
+//}
--- src/companystructureapp/SoftwareEngineer.java
+++ src/companystructureapp/SoftwareEngineer.java
@@ -1,31 +1,31 @@
-package companystructureapp;
-
-public class SoftwareEngineer extends TechnicalEmployee {
-    private boolean codeAccess;
-
-    public SoftwareEngineer(String name) {
-        super(name);
-        this.codeAccess = false;
-    }
-
-    public boolean getCodeAccess() {
-        return codeAccess;
-    }
-
-    public void setCodeAccess(boolean access) {
-        this.codeAccess = access;
-    }
-
-    public int getSuccessfulCheckIns() {
-        return getCheckIns();
-    }
-
-    public boolean checkInCode() {
-
-    }
-
-    @Override
-    public Employee getManager() {
-        return null;
-    }
-}
+//package companystructureapp;
+//
+//public class SoftwareEngineer extends TechnicalEmployee {
+//    private boolean codeAccess;
+//
+//    public SoftwareEngineer(String name) {
+//        super(name);
+//        this.codeAccess = false;
+//    }
+//
+//    public boolean getCodeAccess() {
+//        return codeAccess;
+//    }
+//
+//    public void setCodeAccess(boolean access) {
+//        this.codeAccess = access;
+//    }
+//
+//    public int getSuccessfulCheckIns() {
+//        return getCheckIns();
+//    }
+//
+//    public boolean checkInCode() {
+//
+//    }
+//
+//    @Override
+//    public Employee getManager() {
+//        return null;
+//    }
+//}
