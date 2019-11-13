86fc27f83b18d1a8ae8a1de860038b8d75a83cec
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-13 15:21:37 +0200

    Implemented Ninja Cat class

--- src/animalkingdomapp/NinjaCat.java
+++ src/animalkingdomapp/NinjaCat.java
@@ -1,4 +1,31 @@
 package animalkingdomapp;
 
+import java.awt.*;
+
 public class NinjaCat extends Critter {
+    public NinjaCat() {
+    }
+
+    @Override
+    public Action getMove(CritterInfo info) {
+        if(info.frontThreat() || info.backThreat()) {
+            return Action.INFECT;
+        } else if (info.getFront() == Neighbor.WALL) {
+            return Action.RIGHT;
+        } else if (info.getFront() == Neighbor.SAME) {
+            return Action.LEFT;
+        } else {
+            return Action.HOP;
+        }
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.YELLOW;
+    }
+
+    @Override
+    public String toString() {
+        return "#";
+    }
 }
