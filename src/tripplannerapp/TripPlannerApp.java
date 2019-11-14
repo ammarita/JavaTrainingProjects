99e2d0bd0527ae331bd3c5a89f69720520993d93
Marita Lasmane <marita.lasmane@accenture.com>
2019-11-14 12:12:22 +0200

    Created new trip planning project

---
+++ src/tripplannerapp/TripPlannerApp.java
@@ -0,0 +1,12 @@
+package tripplannerapp;
+/*
+For this project, you are going to write a program that asks the user for some information about an international trip
+they are taking. Based on that information you will need to do some conversions,
+using the correct data types, to tell them some information to help them plan their trip.
+ */
+
+public class TripPlannerApp {
+    public static void main(String[] args) {
+
+    }
+}
